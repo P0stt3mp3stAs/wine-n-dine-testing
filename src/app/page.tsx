cccb2276fb5be4e6dabca5ec760b6d77b1600a2e
myMac <ghaliwali@gmail.com>
2024-11-13 16:43:58 +0100

    menu item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-
 import Bar from "@/components/Bar";
+import MenuItem from "@/components/MenuItem";
 
 export default function Home() {
   return (
@@ -7,6 +7,12 @@ export default function Home() {
       hello worls
       <Bar/>
       ...........................e
+      <MenuItem 
+        image="/menu/des-002.jpg"
+        name="BSK Sundae"
+        description="Chocolate, raspberry, lime Chantilly"
+        price={12.00}
+      />
     </div>
   );
 }
