93f85eb2ab5ea73c35348ab9f1b7e4830e4f1e28
myMac <ghaliwali@gmail.com>
2024-11-21 14:30:32 +0100

    deploy again

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,70 +1,70 @@
-'use client'
+// 'use client'
 
-import React, { useRef, useEffect } from 'react'
-import { Canvas, useThree } from '@react-three/fiber'
-import { useGLTF } from '@react-three/drei'
-import * as THREE from 'three'
-import { Group } from 'three'
-import SceneLighting from './SceneLighting'
+// import React, { useRef, useEffect } from 'react'
+// import { Canvas, useThree } from '@react-three/fiber'
+// import { useGLTF } from '@react-three/drei'
+// import * as THREE from 'three'
+// import { Group } from 'three'
+// import SceneLighting from './SceneLighting'
 
-useGLTF.preload('/models/xybar2.gltf')
+// useGLTF.preload('/models/xybar2.gltf')
 
-function ModelWithLights() {
-  const { scene } = useGLTF('/models/xybar2.gltf')
-  const groupRef = useRef<Group>(null)
+// function ModelWithLights() {
+//   const { scene } = useGLTF('/models/xybar2.gltf')
+//   const groupRef = useRef<Group>(null)
 
-  const position = {
-    x: 0,
-    y: -5,
-    z: 0
-  }
+//   const position = {
+//     x: 0,
+//     y: -5,
+//     z: 0
+//   }
 
-  const rotation = {
-    x: THREE.MathUtils.degToRad(0),
-    y: THREE.MathUtils.degToRad(-45),
-    z: THREE.MathUtils.degToRad(2)
-  }
+//   const rotation = {
+//     x: THREE.MathUtils.degToRad(0),
+//     y: THREE.MathUtils.degToRad(-45),
+//     z: THREE.MathUtils.degToRad(2)
+//   }
 
-  const scale = {
-    x: 6,
-    y: 6,
-    z: 6
-  }
+//   const scale = {
+//     x: 6,
+//     y: 6,
+//     z: 6
+//   }
 
-  useEffect(() => {
-    if (groupRef.current) {
-      groupRef.current.scale.set(scale.x, scale.y, scale.z)
-      groupRef.current.rotation.set(rotation.x, rotation.y, rotation.z)
-      groupRef.current.position.set(position.x, position.y, position.z)
-    }
-  }, [])
+//   useEffect(() => {
+//     if (groupRef.current) {
+//       groupRef.current.scale.set(scale.x, scale.y, scale.z)
+//       groupRef.current.rotation.set(rotation.x, rotation.y, rotation.z)
+//       groupRef.current.position.set(position.x, position.y, position.z)
+//     }
+//   }, [])
 
-  return (
-    <group ref={groupRef}>
-      <primitive object={scene} />
-      <SceneLighting showLightHelpers={true} />
-    </group>
-  )
-}
+//   return (
+//     <group ref={groupRef}>
+//       <primitive object={scene} />
+//       <SceneLighting showLightHelpers={true} />
+//     </group>
+//   )
+// }
 
-function CameraController() {
-  const { camera } = useThree()
+// function CameraController() {
+//   const { camera } = useThree()
   
-  useEffect(() => {
-    camera.position.set(0, 0, 15)
-    camera.updateProjectionMatrix()
-  }, [camera])
+//   useEffect(() => {
+//     camera.position.set(0, 0, 15)
+//     camera.updateProjectionMatrix()
+//   }, [camera])
 
-  return null
-}
+//   return null
+// }
 
-export default function Bar() {
-  return (
-    <div style={{ width: '100%', height: '900px', position: 'relative' }}>
-      <Canvas shadows>
-        <CameraController />
-        <ModelWithLights />
-      </Canvas>
-    </div>
-  )
-}
+// export default function Bar() {
+//   return (
+//     <div style={{ width: '100%', height: '900px', position: 'relative' }}>
+//       <Canvas shadows>
+//         <CameraController />
+//         <ModelWithLights />
+//       </Canvas>
+//     </div>
+//   )
+// }
