ab58091595075fea7dda007b4f3f4d3b56ef8e60
myMac <ghaliwali@gmail.com>
2024-11-27 13:45:41 +0100

    testing what load faster gltf or glb

---
+++ src/components/Right.jsx
@@ -0,0 +1,40 @@
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { useGLTF } from '@react-three/drei';
+
+function Model3D({ position, scale, rotation }) {
+  const gltf = useGLTF('/models/bar23.glb');
+  return (
+    <primitive 
+      object={gltf.scene} 
+      scale={scale} 
+      position={position}
+      rotation={rotation} // Add rotation in radians [x, y, z]
+    />
+  );
+}
+
+export default function Right({ 
+  position = [0, -1, 0], // Default position [x, y, z]
+  scale = 5, // Default scale
+  rotation = [0, 0, 0] // Default rotation in radians [x, y, z]
+}) {
+  return (
+    <div className="h-[384px] w-4/5 bg-green-400">
+        glb
+      <Canvas 
+        camera={{ position: [0, 2, 15], fov: 45 }}
+      >
+        <ambientLight intensity={0.7} />
+        <directionalLight position={[10, 10, 5]} intensity={1.2} />
+        <Suspense fallback={null}>
+          <Model3D 
+            position={position} 
+            scale={scale} 
+            rotation={rotation} 
+          />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
