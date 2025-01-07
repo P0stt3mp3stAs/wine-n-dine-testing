012b2c62630c1a2cce3ae301b2ae84b8e8865ef1
myMac <ghaliwali@gmail.com>
2025-01-07 23:45:16 +0100

    debugged again

--- src/lib/server-auth.ts
+++ src/lib/server-auth.ts
@@ -6,7 +6,6 @@ Amplify.configure({
     Cognito: {
       userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID!,
       userPoolClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID!,
-      region: process.env.NEXT_PUBLIC_AWS_REGION!
     }
   }
 }, {
