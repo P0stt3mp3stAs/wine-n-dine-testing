7d6e6c8129e38c7a87df7b04367cae0fa6164b12
myMac <ghaliwali@gmail.com>
2025-01-09 00:37:43 +0100

    no seats

--- next.config.js
+++ next.config.js
@@ -1,24 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['my-app-user-profiles.s3.us-east-1.amazonaws.com']
-  },
-  eslint: {
-    ignoreDuringBuilds: true
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        pg: false,
-        fs: false,
-        net: false,
-        tls: false,
-        dns: false,
-      };
+    eslint: {
+      ignoreDuringBuilds: true
     }
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+  };
+  
+  module.exports = nextConfig;
